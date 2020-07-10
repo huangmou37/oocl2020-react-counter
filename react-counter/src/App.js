5cb2e526d69307802fffbb374a9c55124c89e06d
Cheng Huang <cheng.huang2@exxonmobil.com>
2020-07-10 14:30:26 +0800

    Init project react-counter

---
+++ react-counter/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 0 };
+  }
+
+  render() {
+    return <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  };
+}
+
+export default App;
